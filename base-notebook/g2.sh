b260d2615e61cbf65ebcb01e1c1822e495546161
Alexei Karve <karve@us.ibm.clom>
2021-05-17 13:44:02 +0000

    Fixes

--- base-notebook/g2.sh
+++ base-notebook/g2.sh
@@ -6,17 +6,20 @@ export http_proxy="http:$PROXY_URL"
 export https_proxy="http:$PROXY_URL"
 export ftp_proxy="ftp:$PROXY_URL"
 export no_proxy="10.3.158.0/24,127.0.0.1,localhost"
-#podman build --tls-verify=false -t default-route-openshift-image-registry.apps.ibm-hcs.priv/$YOUR_NAMESPACE/base-notebook-tensorflow -f Dockerfile.ppc64le.tensorflow .
+podman build --tls-verify=false -t default-route-openshift-image-registry.apps.ibm-hcs.priv/$YOUR_NAMESPACE/base-notebook-tensorflow -f Dockerfile.ppc64le.tensorflow .
 #podman build --layers=false --tls-verify=false -t default-route-openshift-image-registry.apps.ibm-hcs.priv/$YOUR_NAMESPACE/base-notebook-tensorflow -f Dockerfile.ppc64le.tensorflow .
-podman build --squash-all --tls-verify=false -t default-route-openshift-image-registry.apps.ibm-hcs.priv/$YOUR_NAMESPACE/base-notebook-tensorflow -f Dockerfile.ppc64le.tensorflow .
-echo Result=$?
-echo Logging in to registry
-unset http_proxy
-unset https_proxy
-podman login --tls-verify=false -u kubeadmin default-route-openshift-image-registry.apps.ibm-hcs.priv -p `cat oc_token` 2>&1 >/dev/null
-if [ $? -ne 0 ]; then
-  oc whoami -t > oc_token
-  podman login --tls-verify=false -u kubeadmin default-route-openshift-image-registry.apps.ibm-hcs.priv -p `cat oc_token`
+#podman build --squash-all --tls-verify=false -t default-route-openshift-image-registry.apps.ibm-hcs.priv/$YOUR_NAMESPACE/base-notebook-tensorflow -f Dockerfile.ppc64le.tensorflow .
+result=$?
+echo Result=$result
+if [ $result -eq 0 ]; then
+ echo Logging in to registry
+ unset http_proxy
+ unset https_proxy
+ podman login --tls-verify=false -u kubeadmin default-route-openshift-image-registry.apps.ibm-hcs.priv -p `cat oc_token` 2>&1 >/dev/null
+ if [ $? -ne 0 ]; then
+   oc whoami -t > oc_token
+   podman login --tls-verify=false -u kubeadmin default-route-openshift-image-registry.apps.ibm-hcs.priv -p `cat oc_token`
+ fi
+ echo Pushing
+ podman push --tls-verify=false default-route-openshift-image-registry.apps.ibm-hcs.priv/$YOUR_NAMESPACE/base-notebook-tensorflow
 fi
-echo Pushing
-podman push --tls-verify=false default-route-openshift-image-registry.apps.ibm-hcs.priv/$YOUR_NAMESPACE/base-notebook-tensorflow
--- base-notebook/gtensorflowubuntu.sh
+++ base-notebook/gtensorflowubuntu.sh
@@ -6,8 +6,8 @@ export http_proxy="http:$PROXY_URL"
 export https_proxy="http:$PROXY_URL"
 export ftp_proxy="ftp:$PROXY_URL"
 export no_proxy="10.3.158.0/24,127.0.0.1,localhost"
-podman build --tls-verify=false -t default-route-openshift-image-registry.apps.ibm-hcs.priv/$YOUR_NAMESPACE/base-notebook-tensorflow3 -f Dockerfile.ppc64le.tensorflow3 .
-#podman build --squash-all --tls-verify=false -t default-route-openshift-image-registry.apps.ibm-hcs.priv/$YOUR_NAMESPACE/base-notebook-tensorflow3 -f Dockerfile.ppc64le.tensorflow3 .
+podman build --tls-verify=false -t default-route-openshift-image-registry.apps.ibm-hcs.priv/$YOUR_NAMESPACE/base-notebook-tensorflowubuntu -f Dockerfile.ppc64le.tensorflowubuntu .
+#podman build --squash-all --tls-verify=false -t default-route-openshift-image-registry.apps.ibm-hcs.priv/$YOUR_NAMESPACE/base-notebook-tensorflowubuntu -f Dockerfile.ppc64le.tensorflowubuntu .
 echo Result=$?
 echo Logging in to registry
 unset http_proxy
@@ -18,4 +18,4 @@ if [ $? -ne 0 ]; then
   podman login --tls-verify=false -u kubeadmin default-route-openshift-image-registry.apps.ibm-hcs.priv -p `cat oc_token`
 fi
 echo Pushing
-podman push --tls-verify=false default-route-openshift-image-registry.apps.ibm-hcs.priv/$YOUR_NAMESPACE/base-notebook-tensorflow3
+podman push --tls-verify=false default-route-openshift-image-registry.apps.ibm-hcs.priv/$YOUR_NAMESPACE/base-notebook-tensorflowubuntu
