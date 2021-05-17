8c1a1a18847dbb28a62f0c5bd4c09ab6171b04a8
Alexei Karve <karve@us.ibm.clom>
2021-05-17 15:19:16 +0000

    Fixes

--- base-notebook/gtensorflow3.sh
+++ base-notebook/gtensorflow3.sh
@@ -8,14 +8,19 @@ export ftp_proxy="ftp:$PROXY_URL"
 export no_proxy="10.3.158.0/24,127.0.0.1,localhost"
 podman build --tls-verify=false -t default-route-openshift-image-registry.apps.ibm-hcs.priv/$YOUR_NAMESPACE/base-notebook-tensorflow3 -f Dockerfile.ppc64le.tensorflow3 .
 #podman build --squash-all --tls-verify=false -t default-route-openshift-image-registry.apps.ibm-hcs.priv/$YOUR_NAMESPACE/base-notebook-tensorflow3 -f Dockerfile.ppc64le.tensorflow3 .
-echo Result=$?
-echo Logging in to registry
-unset http_proxy
-unset https_proxy
-podman login --tls-verify=false -u kubeadmin default-route-openshift-image-registry.apps.ibm-hcs.priv -p `cat oc_token` 2>&1 >/dev/null
-if [ $? -ne 0 ]; then
-  oc whoami -t > oc_token
-  podman login --tls-verify=false -u kubeadmin default-route-openshift-image-registry.apps.ibm-hcs.priv -p `cat oc_token`
+result=$?
+echo Result=$result
+if [ $result -eq 0 ]; then
+ echo Logging in to registry
+ unset http_proxy
+ unset https_proxy
+ podman login --tls-verify=false -u kubeadmin default-route-openshift-image-registry.apps.ibm-hcs.priv -p `cat oc_token` 2>&1 >/dev/null
+ if [ $? -ne 0 ]; then
+   oc whoami -t > oc_token
+   podman login --tls-verify=false -u kubeadmin default-route-openshift-image-registry.apps.ibm-hcs.priv -p `cat oc_token`
+ fi
+ echo Pushing
+ podman push --tls-verify=false default-route-openshift-image-registry.apps.ibm-hcs.priv/$YOUR_NAMESPACE/base-notebook-tensorflow3
+else
+  echo Error: Not Pushed
 fi
-echo Pushing
-podman push --tls-verify=false default-route-openshift-image-registry.apps.ibm-hcs.priv/$YOUR_NAMESPACE/base-notebook-tensorflow3
