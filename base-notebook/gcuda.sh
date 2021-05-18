2534e2e6d4d6ac080d166015473d47347609e054
Alexei Karve <karve@us.ibm.clom>
2021-05-18 13:55:39 +0000

    Add cuda

---
+++ base-notebook/gcuda.sh
@@ -0,0 +1,26 @@
+YOUR_NAMESPACE=default
+oc project $YOUR_NAMESPACE
+echo Building
+PROXY_URL="//10.3.0.3:3128"
+export http_proxy="http:$PROXY_URL"
+export https_proxy="http:$PROXY_URL"
+export ftp_proxy="ftp:$PROXY_URL"
+export no_proxy="10.3.158.0/24,127.0.0.1,localhost"
+podman build --tls-verify=false -t default-route-openshift-image-registry.apps.ibm-hcs.priv/$YOUR_NAMESPACE/base-notebook-cuda -f Dockerfile.ppc64le.cuda .
+#podman build --squash-all --tls-verify=false -t default-route-openshift-image-registry.apps.ibm-hcs.priv/$YOUR_NAMESPACE/base-notebook-cuda -f Dockerfile.ppc64le.cuda .
+result=$?
+echo Result=$result
+if [ $result -eq 0 ]; then
+ echo Logging in to registry
+ unset http_proxy
+ unset https_proxy
+ podman login --tls-verify=false -u kubeadmin default-route-openshift-image-registry.apps.ibm-hcs.priv -p `cat oc_token` 2>&1 >/dev/null
+ if [ $? -ne 0 ]; then
+   oc whoami -t > oc_token
+   podman login --tls-verify=false -u kubeadmin default-route-openshift-image-registry.apps.ibm-hcs.priv -p `cat oc_token`
+ fi
+ echo Pushing
+ podman push --tls-verify=false default-route-openshift-image-registry.apps.ibm-hcs.priv/$YOUR_NAMESPACE/base-notebook-cuda
+else
+  echo Error: Not Pushed
+fi
