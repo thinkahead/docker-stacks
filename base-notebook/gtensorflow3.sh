51057fb5558fe2a503a8b02ffac8194d485e7e36
Alexei Karve <karve@us.ibm.com>
2021-08-31 17:27:49 -0400

    Garage Cluster

--- base-notebook/gtensorflow3.sh
+++ base-notebook/gtensorflow3.sh
@@ -1,26 +1,19 @@
-YOUR_NAMESPACE=default
+YOUR_NAMESPACE=madi
 oc project $YOUR_NAMESPACE
 echo Building
-PROXY_URL="//10.3.0.3:3128"
-export http_proxy="http:$PROXY_URL"
-export https_proxy="http:$PROXY_URL"
-export ftp_proxy="ftp:$PROXY_URL"
-export no_proxy="10.3.158.0/24,127.0.0.1,localhost"
-podman build --tls-verify=false -t default-route-openshift-image-registry.apps.ibm-hcs.priv/$YOUR_NAMESPACE/base-notebook-tensorflow3 -f Dockerfile.ppc64le.tensorflow3 .
-#podman build --squash-all --tls-verify=false -t default-route-openshift-image-registry.apps.ibm-hcs.priv/$YOUR_NAMESPACE/base-notebook-tensorflow3 -f Dockerfile.ppc64le.tensorflow3 .
+podman build --tls-verify=false -t default-route-openshift-image-registry.apps.ies.pbm.ihost.com/$YOUR_NAMESPACE/base-notebook-tensorflow3 -f Dockerfile.ppc64le.tensorflow3 .
+#podman build --squash-all --tls-verify=false -t default-route-openshift-image-registry.apps.ies.pbm.ihost.com/$YOUR_NAMESPACE/base-notebook-tensorflow3 -f Dockerfile.ppc64le.tensorflow3 .
 result=$?
 echo Result=$result
 if [ $result -eq 0 ]; then
  echo Logging in to registry
- unset http_proxy
- unset https_proxy
- podman login --tls-verify=false -u kubeadmin default-route-openshift-image-registry.apps.ibm-hcs.priv -p `cat oc_token` 2>&1 >/dev/null
+ podman login --tls-verify=false -u kubeadmin default-route-openshift-image-registry.apps.ies.pbm.ihost.com -p `cat oc_token` 2>&1 >/dev/null
  if [ $? -ne 0 ]; then
    oc whoami -t > oc_token
-   podman login --tls-verify=false -u kubeadmin default-route-openshift-image-registry.apps.ibm-hcs.priv -p `cat oc_token`
+   podman login --tls-verify=false -u kubeadmin default-route-openshift-image-registry.apps.ies.pbm.ihost.com -p `cat oc_token`
  fi
  echo Pushing
- podman push --tls-verify=false default-route-openshift-image-registry.apps.ibm-hcs.priv/$YOUR_NAMESPACE/base-notebook-tensorflow3
+ podman push --tls-verify=false default-route-openshift-image-registry.apps.ies.pbm.ihost.com/$YOUR_NAMESPACE/base-notebook-tensorflow3
 else
   echo Error: Not Pushed
 fi
